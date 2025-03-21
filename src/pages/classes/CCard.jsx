cb8fd253b3caad8666dfd1fc971714428fbf91e1
Michele <mikzed@hotmail.it>
2025-03-21 13:42:58 +0100

    class card adjusted

--- src/pages/classes/CCard.jsx
+++ src/pages/classes/CCard.jsx
@@ -11,11 +11,15 @@ function CCard({ c }) {
     <>
       <div className="relative">
         <img src={c.img} alt={c.name} className="opacity-30 rounded-lg" />
-        <div className="absolute bottom-5 text-center gap-2 flex flex-col p-2">
-          <h4 className="text-xl xl:text-2xl font-semibold">{c.name}</h4>
-          <p className="text-sm sm:text-lg">{c.description}</p>
+        <div className="flex text-center gap-2 flex-col p-2">
+          <h4 className="absolute top-3 left-1/2 -translate-x-1/2 text-xl xl:text-2xl font-semibold">
+            {c.name}
+          </h4>
+          <p className="absolute top-10 w-full p-1 left-1/2 -translate-x-1/2 text-sm sm:text-lg">
+            {c.description}
+          </p>
           <button
-            className="text-neutral-950 bg-neutral-400 hover:bg-lime-600 duration-300 rounded-lg px-4 py-2 cursor-pointer active:scale-98 font-semibold mt-2 mx-10 text-sm xl:text-lg"
+            className="absolute bottom-7 left-1/2 -translate-x-1/2 w-[90%] text-neutral-950 bg-neutral-400 hover:bg-lime-600 duration-300 rounded-lg py-2 cursor-pointer active:scale-98 font-semibold text-xs xl:text-lg"
             onClick={handleClick}
           >
             Schedule Now
