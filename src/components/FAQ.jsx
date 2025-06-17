8865101fe8084f2e04b3a8a36a2baa4db77ef1c7
Michele <mikzed@hotmail.it>
2025-06-17 11:17:13 +0200

    margin fixed on faq component

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -11,7 +11,7 @@ function FAQ() {
 
   return (
     <>
-      <div className="flex flex-col place-items-center mt-30 xl:mt-40 bg-neutral-700 pt-25 md:pt-55 pb-45 md:pb-75 skew-y-10">
+      <div className="flex flex-col place-items-center mt-30 xl:mt-40 sm:mb-30 lg:mb-60 bg-neutral-700 pt-25 md:pt-55 pb-45 md:pb-75 skew-y-10">
         <div className="mx-auto max-w-screen-lg -skew-y-10 flex flex-col items-center justify-center">
           <h3 className="text-5xl lg:text-6xl text-neutral-950 font-bold text-center">
             FAQ
