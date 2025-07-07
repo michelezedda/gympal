a96e7a8e3b84f93a99e06bcf043c8c37e220023b
Michele <mikzed@hotmail.it>
2025-07-07 12:36:41 +0200

    carousel implemented

--- src/components/trainers/TrainerCard.jsx
+++ src/components/trainers/TrainerCard.jsx
@@ -2,11 +2,11 @@ function TrainersCard({ trainer }) {
   return (
     <>
       <div className="flex relative">
-        <img src="/media/frame.png" />
+        <img src="/media/frame.png" className="pl-[0.5px] h-100 lg:h-120" />
         <img
           src={trainer.pic}
           alt={trainer.name}
-          className="absolute left-1/2 bottom-0 -translate-x-1/2 h-[130%] xl:h-90 grayscale-100 hover:grayscale-0 duration-200 cursor-pointer"
+          className="absolute left-1/2 -translate-x-1/2 bottom-0 h-full grayscale-100 hover:grayscale-0 duration-200"
         />
         <div className="flex flex-col absolute left-1/2 -bottom-5 -translate-x-1/2 bg-neutral-800 w-full py-2 text-center">
           <span className="font-semibold text-xl">
--- src/components/trainers/Trainers.jsx
+++ src/components/trainers/Trainers.jsx
@@ -1,20 +1,56 @@
+import { useState } from "react";
 import trainers from "../../data/trainers";
 import TrainerCard from "./TrainerCard";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 
 function Trainers() {
+  const [currentTrainer, setCurrentTrainer] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentTrainer(
+      currentTrainer === 0 ? trainers.length - 1 : currentTrainer - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentTrainer(
+      currentTrainer === trainers.length - 1 ? 0 : currentTrainer + 1
+    );
+  };
+
   return (
     <>
       <div className="container mx-auto max-w-screen-lg">
         <div className="flex flex-col place-items-center mt-20">
-          <h3 className="text-4xl sm:text-5xl lg:text-6xl text-lime-600 font-bold mb-15 xl:mb-6 2xl:mb-20">
+          <h3 className="text-4xl sm:text-5xl lg:text-6xl text-lime-600 font-bold mb-10">
             MEET OUR TRAINERS
           </h3>
-          <div className="grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-2 gap-y-40 xl:gap-y-15 2xl:gap-y-50 mt-20">
-            {trainers.map((trainer) => (
-              <div key={trainer.id}>
-                <TrainerCard trainer={trainer} />
-              </div>
-            ))}
+          <div className="relative">
+            <IoIosArrowDropleftCircle
+              className="absolute left-0 bottom-1/2 cursor-pointer z-1 text-white bg-neutral-800 hover:bg-lime-600 duration-200 ease-in-out rounded-full"
+              size={55}
+              onClick={handlePrev}
+            />
+            <div className="flex mx-6 mt-10">
+              {trainers.map((trainer, index) => (
+                <div
+                  key={trainer.id}
+                  className={`${
+                    currentTrainer === index ? "visible" : "hidden"
+                  }`}
+                >
+                  <TrainerCard trainer={trainer} />
+                </div>
+              ))}
+            </div>
+            <IoIosArrowDroprightCircle
+              className="absolute right-0 bottom-1/2 cursor-pointer z-1 text-white bg-neutral-800 hover:bg-lime-600 duration-200 ease-in-out rounded-full"
+              size={55}
+              onClick={handleNext}
+            />
           </div>
         </div>
       </div>
