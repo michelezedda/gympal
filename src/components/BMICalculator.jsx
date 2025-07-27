4feadb58b5a3942c3f01e8e212f25b3d26ecf733
Michele <mikzed@hotmail.it>
2025-07-27 11:31:41 +0200

    metric systems added

--- src/components/BMICalculator.jsx
+++ src/components/BMICalculator.jsx
@@ -1,21 +1,47 @@
 import { useState } from "react";
 
 function BMICalculator() {
+  const [unit, setUnit] = useState("imperial");
   const [imperialForm, setImperialForm] = useState({
     feet: "",
     inches: "",
     pounds: "",
   });
+  const [metricForm, setMetricForm] = useState({
+    centimeters: "",
+    kilograms: "",
+  });
   const [BMI, setBMI] = useState(null);
 
   const calculateBMI = () => {
-    const feet = Number(imperialForm.feet);
-    const inches = Number(imperialForm.inches);
-    const pounds = Number(imperialForm.pounds);
+    if (unit === "imperial") {
+      const feet = Number(imperialForm.feet);
+      const inches = Number(imperialForm.inches);
+      const pounds = Number(imperialForm.pounds);
 
-    const totalInches = feet * 12 + inches;
-    const bmi = (pounds * 703) / totalInches ** 2;
-    setBMI(bmi.toFixed(1));
+      const totalInches = feet * 12 + inches;
+      const bmi = (pounds * 703) / totalInches ** 2;
+      setBMI(bmi.toFixed(1));
+      setImperialForm({
+        feet: "",
+        inches: "",
+        pounds: "",
+      });
+    } else {
+      const cm = Number(metricForm.centimeters);
+      const kg = Number(metricForm.kilograms);
+
+      const bmi = kg / (cm / 100) ** 2;
+      setBMI(bmi.toFixed(1));
+      setMetricForm({
+        centimeters: "",
+        kilograms: "",
+      });
+    }
+  };
+
+  const handleUnit = () => {
+    setUnit(unit === "imperial" ? "metric" : "imperial");
   };
 
   const handleSubmit = (e) => {
@@ -30,61 +56,103 @@ function BMICalculator() {
           <h3 className="text-4xl lg:text-6xl text-lime-600 font-bold mb-8">
             CALCULATE YOUR BMI
           </h3>
-          <form
-            className="flex flex-col justify-center gap-2 w-full"
-            onSubmit={handleSubmit}
+          <button
+            onClick={handleUnit}
+            className="hover:bg-lime-600 hover:text-neutral-950 duration-300 active:scale-98 cursor-pointer shadow-black shadow-md py-2 px-4 mt-2 font-semibold bg-neutral-800 w-full mb-2"
           >
-            <input
-              type="number"
-              name="feet"
-              placeholder="feet"
-              value={imperialForm.feet}
-              onChange={(e) =>
-                setImperialForm({ ...imperialForm, feet: e.target.value })
-              }
-              required
-              className="flex flex-col gap-2 border-4 p-4 cursor-pointer border-neutral-500"
-            />
-            <input
-              type="number"
-              name="inches"
-              placeholder="inches"
-              value={imperialForm.inches}
-              onChange={(e) =>
-                setImperialForm({
-                  ...imperialForm,
-                  inches: e.target.value,
-                })
-              }
-              required
-              className="flex flex-col gap-2 border-4 p-4 cursor-pointer border-neutral-500"
-            />
-            <input
-              type="number"
-              name="pounds"
-              placeholder="pounds"
-              value={imperialForm.pounds}
-              onChange={(e) =>
-                setImperialForm({
-                  ...imperialForm,
-                  pounds: e.target.value,
-                })
-              }
-              required
-              className="flex flex-col gap-2 border-4 p-4 cursor-pointer border-neutral-500 w-full"
-            />
-            <button
-              className={`hover:bg-lime-600 hover:text-neutral-950 duration-300 active:scale-98 cursor-pointer shadow-black shadow-md py-2 px-4 mt-2 font-semibold bg-neutral-800`}
+            {unit === "imperial"
+              ? "CHANGE TO METRIC UNITS"
+              : "CHANGE TO US UNITS"}
+          </button>
+          {unit === "imperial" ? (
+            <form
+              className="flex flex-col justify-center gap-2 w-full"
+              onSubmit={handleSubmit}
             >
-              CALCULATE
-            </button>
-          </form>
+              <input
+                type="number"
+                name="feet"
+                placeholder="feet"
+                value={imperialForm.feet}
+                onChange={(e) =>
+                  setImperialForm({ ...imperialForm, feet: e.target.value })
+                }
+                required
+                className="flex gap-2 border-4 p-4 cursor-pointer border-neutral-500"
+              />
+              <input
+                type="number"
+                name="inches"
+                placeholder="inches"
+                value={imperialForm.inches}
+                onChange={(e) =>
+                  setImperialForm({
+                    ...imperialForm,
+                    inches: e.target.value,
+                  })
+                }
+                required
+                className="flex gap-2 border-4 p-4 cursor-pointer border-neutral-500"
+              />
+              <input
+                type="number"
+                name="pounds"
+                placeholder="pounds"
+                value={imperialForm.pounds}
+                onChange={(e) =>
+                  setImperialForm({
+                    ...imperialForm,
+                    pounds: e.target.value,
+                  })
+                }
+                required
+                className="flex gap-2 border-4 p-4 cursor-pointer border-neutral-500 w-full"
+              />
+              <button className="hover:bg-lime-600 hover:text-neutral-950 duration-300 active:scale-98 cursor-pointer shadow-black shadow-md py-2 px-4 mt-2 font-semibold bg-neutral-800">
+                CALCULATE
+              </button>
+            </form>
+          ) : (
+            <form
+              className="flex flex-col justify-center gap-2 w-full"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="number"
+                name="centimeters"
+                placeholder="centimeters"
+                value={metricForm.centimeters}
+                onChange={(e) =>
+                  setMetricForm({ ...metricForm, centimeters: e.target.value })
+                }
+                required
+                className="flex gap-2 border-4 p-4 cursor-pointer border-neutral-500"
+              />
+              <input
+                type="number"
+                name="kilograms"
+                placeholder="kilograms"
+                value={metricForm.kilograms}
+                onChange={(e) =>
+                  setMetricForm({
+                    ...metricForm,
+                    kilograms: e.target.value,
+                  })
+                }
+                required
+                className="flex gap-2 border-4 p-4 cursor-pointer border-neutral-500"
+              />
+              <button className="hover:bg-lime-600 hover:text-neutral-950 duration-300 active:scale-98 cursor-pointer shadow-black shadow-md py-2 px-4 mt-2 font-semibold bg-neutral-800">
+                CALCULATE
+              </button>
+            </form>
+          )}
           {BMI ? (
-            <div className="mt-4 flex flex-col items-center">
+            <div className="mt-4 flex flex-col items-center w-full">
               <h4 className="my-4 text-3xl lg:text-4xl font-semibold text-neutral-300">
                 Your BMI: <span className="font-bold text-lime-600">{BMI}</span>
               </h4>
-              <div className="flex flex-col w-100 gap-2 text-xl lg:text-2xl">
+              <div className="flex flex-col gap-2 text-xl lg:text-2xl w-full">
                 <p
                   className={`py-2 px-8 flex justify-between ${
                     BMI < 18.5
